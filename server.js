448bb1116542557a3ddc9360a16ce599f046ba58
Daniel Hawkes <dan@danhawkes.co.uk>
2015-01-17 19:12:28 +0000

    more

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ var request = require('request');
 
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.static('dist'));
 app.use(morgan('dev'))
 app.use(cookieParser());
@@ -21,6 +26,7 @@ passport.use(new LocalStrategy(
     authenticateUser(username, password, done);
   }
 ));
+
 
 app.post('/register', function(req, res) {
   var username = req.body.username;
