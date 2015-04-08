b8ab5e053bf87a87af0418da08c22021f26255ac
Daniel Hawkes <dan@danhawkes.co.uk>
2015-04-08 23:58:57 +0100

    Merge config env vars for brevity.

--- gulpconfig.js
+++ gulpconfig.js
@@ -1,6 +1,4 @@
 // These configure where the app looks for the login/registration services.
 module.exports = [
-  {pattern: 'CONFIG_WEB_PROTOCOL', replacement: process.env.WEB_PROTOCOL || 'http'},
-  {pattern: 'CONFIG_WEB_HOST', replacement: process.env.WEB_HOST || 'tracer.arcs.co'},
-  {pattern: 'CONFIG_WEB_PORT', replacement: process.env.WEB_PORT || '80'}
+  {pattern: 'CONFIG_WEB_URL', replacement: process.env.WEB_URL || 'http://tracer.arcs.co'}
 ];
